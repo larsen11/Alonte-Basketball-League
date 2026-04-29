2ed09436149e6968f761e1fe3213f8d826edee72
larsen11 <130536493+larsen11@users.noreply.github.com>
2026-04-29 13:17:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,11 @@ mongoose
     const submitForm = require('./API/submit')
 
     app.use("/submit", submitForm);
-const PORT = 5000;
+const PORT = process.env.PORT || 8080; 
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
